a0d2cd571db54cee24091cec8c1e3b2f50e935a8
jsahdjhkaskdjl <148912349+jsahdjhkaskdjl@users.noreply.github.com>
2023-10-25 14:36:46 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import {
 	Menu,
 	Vector2,
 	RendererSDK,
-	Color
+	Color,
+	LocalPlayer,
+	Hero
 } from "github.com/octarine-public/wrapper/index"
 
 const Entry = Menu.AddEntry("Utility")
@@ -16,14 +18,22 @@ const testTask = Entry.AddNode(
 
 const Enable = testTask.AddToggle("State")
 
-
-
 EventsSDK.on("Draw", () => {
 	if ( Enable.value == true)
 	{
+		const MyHero = LocalPlayer?.Hero
+		console.log(MyHero)
 		const vectorSize = new Vector2(100, 100)
 		const position = new Vector2(300, 300)
 		RendererSDK.FilledCircle(position, vectorSize, Color.Yellow.SetA(255))
+
+		// RendererSDK.Image(
+		// 	`panorama/images/heroes/icons/${value.ally.Name}_png.vtex_c`,
+		// 	position,
+		// 	0,
+		// 	vectorSize,
+		// 	new Color(255, 255, 255, OpactitySlider.value * 2.55)
+		// )
 	}
 
 })
