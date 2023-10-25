4dc969084478e635c1c75c153e04bedfed2c6eac
jsahdjhkaskdjl <148912349+jsahdjhkaskdjl@users.noreply.github.com>
2023-10-25 10:48:48 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,26 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+import {
+	EntityManager,
+	EventsSDK,
+	GameState,
+	item_blink,
+	LocalPlayer,
+	Menu,
+	monkey_king_tree_dance,
+	ParticleAttachment,
+	ParticlesSDK,
+	TickSleeper,
+	Vector3
+} from "github.com/octarine-public/wrapper/index"
+
+
+const Entry = Menu.AddEntry("Utility")
+const testTask = Entry.AddNode(
+	"test 123123",
+	"panorama/images/spellicons/monkey_king_tree_dance_png.vtex_c",
+	undefined,
+	0
+)
 
 EventsSDK.on("GameStarted", () => {
-	console.log("LocalPlayers")
+	console.log("LocalPlayer")
 })
