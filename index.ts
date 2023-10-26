09167bad378d5d3e3a67838114a380cf96b9f666
jsahdjhkaskdjl <148912349+jsahdjhkaskdjl@users.noreply.github.com>
2023-10-26 09:15:03 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,16 +8,16 @@ import {
 	InputManager
 } from "github.com/octarine-public/wrapper/index"
 
-const Entry = Menu.AddEntry("Utility")
+const Entry = Menu.AddEntry("Utility");
 const testTask = Entry.AddNode(
 	"Тестовое задание",
 	undefined,
 	undefined,
 	0
-)
+);
 
-const Enable = testTask.AddToggle("State")
-let position = new Vector2(400,400)
+const Enable = testTask.AddToggle("State");
+let position = new Vector2(400,400);
 
 EventsSDK.on("Draw", () => {
 
@@ -25,25 +25,25 @@ EventsSDK.on("Draw", () => {
 	{	
 
 
-		let test1 = new Vector2(40,40);
-		let test2 = new Vector2(45,45);
-		let test3 = new Vector2(50,50);
-		if ( test2 > test1 && test2 < test3 ){
-			console.log("works!")
-		}
+		// let test1 = new Vector2(40,40);
+		// let test2 = new Vector2(45,45);
+		// let test3 = new Vector2(50,50);
+		// if ( test2 > test1 && test2 < test3 ){
+		console.log("works!");
+		// }
 
-		const MyHero = LocalPlayer?.Hero?.Name
-		console.log(position.Ceil)
+		const MyHero = LocalPlayer?.Hero?.Name;
+		console.log(position.Ceil);
 		if ( InputManager.IsMouseKeyDown(0x01) ){
 			position = InputManager?.CursorOnScreen
-		}
+		};
 
 		RendererSDK.Image(`panorama/images/heroes/icons/${MyHero}_png.vtex_c`,
 			position,
 			0,
 			new Vector2(50,50),
 			new Color(255, 255, 255, 255)
-		)	
+		)	;
 	}
-})
+});
 
