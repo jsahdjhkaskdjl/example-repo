832d3939b66a2300bb2ce8320578fb9badb08d85
jsahdjhkaskdjl <148912349+jsahdjhkaskdjl@users.noreply.github.com>
2023-10-26 09:42:32 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ EventsSDK.on("Draw", () => {
 
 		let cursor_pos = InputManager?.CursorOnScreen; 
 
-		if ( InputManager.IsMouseKeyDown(0x01) && position?.x < cursor_pos?.x && position?.y < cursor_pos?.y && cursor_pos?.x < position?.x + 50 && cursor_pos?.y < position?.y + 50 ){
+		if ( InputManager.IsMouseKeyDown(0x01) && position?.x - 20 < cursor_pos?.x && position?.y - 20 < cursor_pos?.y && cursor_pos?.x < position?.x + 70 && cursor_pos?.y < position?.y + 70 ){
 			position = InputManager?.CursorOnScreen
 		};
 
