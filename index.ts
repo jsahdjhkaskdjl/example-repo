cc68908fe56fa53bac35f1d3db7adf6733bb79ed
jsahdjhkaskdjl <148912349+jsahdjhkaskdjl@users.noreply.github.com>
2023-10-26 09:18:29 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -28,7 +28,7 @@ EventsSDK.on("Draw", () => {
 		let test1 = new Vector2(40,40);
 		let test2 = new Vector2(45,45);
 		// let test3 = new Vector2(50,50);
-		console.log(test1.x)
+		console.log(test1.x.valueOf)
 
 		const MyHero = LocalPlayer?.Hero?.Name;
 		console.log(position.Ceil);
